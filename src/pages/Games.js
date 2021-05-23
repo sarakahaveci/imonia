ada5709cc4eb1d8064aead90590c3391d88170ce
yamoonz <yamooonz@gmail.com>
2021-05-23 21:11:27 +0300

    --- Auto-Git Commit ---

---
+++ src/pages/Games.js
@@ -0,0 +1,153 @@
+import React from "react";
+import Table from "react-bootstrap/Table";
+import './Games.css';
+
+function Games() {
+  return (
+    <div>
+      <h1>Games List</h1>
+      <div class="table-responsive">
+        <table class="table">
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>Benzirsiz Adi</th>
+                <th>Gurusen Isim</th>
+                <th>Aciklama</th>
+                <th>Olusturma Tarihi</th>
+                <th>Bitis Tarihi</th>
+                <th>Oyun Tarihi</th>
+                <th>Oyun Durumu</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>AlGame</td>
+                <td>Activity List Game</td>
+                <td>Activity List Game</td>
+                <td> 01.01.2021</td>
+                <td>01.01.2021</td>
+                <td>Bireyesel</td>
+                <td>Acik</td>
+                <button>Duzenle</button>
+              </tr>
+              <tr>
+                <td>AlGame</td>
+                <td>Activity List Game</td>
+                <td>Activity List Game</td>
+                <td> 01.01.2021</td>
+                <td>01.01.2021</td>
+                <td>Bireyesel</td>
+                <td>Acik</td>
+                <button>Duzenle</button>
+              </tr>
+              <tr>
+                <td>AlGame</td>
+                <td>Activity List Game</td>
+                <td>Activity List Game</td>
+                <td> 01.01.2021</td>
+                <td>01.01.2021</td>
+                <td>Bireyesel</td>
+                <td>Acik</td>
+                <button>Duzenle</button>
+              </tr>
+              <tr>
+                <td>AlGame</td>
+                <td>Activity List Game</td>
+                <td>Activity List Game</td>
+                <td> 01.01.2021</td>
+                <td>01.01.2021</td>
+                <td>Bireyesel</td>
+                <td>Acik</td>
+                <button>Duzenle</button>
+              </tr>
+              <tr>
+                <td>AlGame</td>
+                <td>Activity List Game</td>
+                <td>Activity List Game</td>
+                <td> 01.01.2021</td>
+                <td>01.01.2021</td>
+                <td>Bireyesel</td>
+                <td>Acik</td>
+                <button>Duzenle</button>
+              </tr>
+              <tr>
+                <td>AlGame</td>
+                <td>Activity List Game</td>
+                <td>Activity List Game</td>
+                <td> 01.01.2021</td>
+                <td>01.01.2021</td>
+                <td>Bireyesel</td>
+                <td>Acik</td>
+                <button>Duzenle</button>
+              </tr>
+              <tr>
+                <td>AlGame</td>
+                <td>Activity List Game</td>
+                <td>Activity List Game</td>
+                <td> 01.01.2021</td>
+                <td>01.01.2021</td>
+                <td>Bireyesel</td>
+                <td>Acik</td>
+                <button>Duzenle</button>
+              </tr>
+              <tr>
+                <td>AlGame</td>
+                <td>Activity List Game</td>
+                <td>Activity List Game</td>
+                <td> 01.01.2021</td>
+                <td>01.01.2021</td>
+                <td>Bireyesel</td>
+                <td>Acik</td>
+                <button>Duzenle</button>
+              </tr>
+              <tr>
+                <td>AlGame</td>
+                <td>Activity List Game</td>
+                <td>Activity List Game</td>
+                <td> 01.01.2021</td>
+                <td>01.01.2021</td>
+                <td>Bireyesel</td>
+                <td>Acik</td>
+                <button>Duzenle</button>
+              </tr>
+              <tr>
+                <td>AlGame</td>
+                <td>Activity List Game</td>
+                <td>Activity List Game</td>
+                <td> 01.01.2021</td>
+                <td>01.01.2021</td>
+                <td>Bireyesel</td>
+                <td>Acik</td>
+                <button>Duzenle</button>
+              </tr>
+              <tr>
+                <td>AlGame</td>
+                <td>Activity List Game</td>
+                <td>Activity List Game</td>
+                <td> 01.01.2021</td>
+                <td>01.01.2021</td>
+                <td>Bireyesel</td>
+                <td>Acik</td>
+                <button>Duzenle</button>
+              </tr>
+              <tr>
+                <td>AlGame</td>
+                <td>Activity List Game</td>
+                <td>Activity List Game</td>
+                <td> 01.01.2021</td>
+                <td>01.01.2021</td>
+                <td>Bireyesel</td>
+                <td>Acik</td>
+                <button>Duzenle</button>
+              </tr>
+            </tbody>
+          </Table>
+          <button> Yeni Oyna Ekle </button>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Games;
