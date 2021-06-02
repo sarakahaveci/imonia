33d833b11bd95241db2d4a28a2546946d7a094e8
yamoonz <yamooonz@gmail.com>
2021-06-02 17:11:06 +0300

    fixed navbar toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,43 +7,40 @@ import './Navbar.css';
 import { IconContext } from 'react-icons';
 
 function Navbar() {
-    const [sidebar, setSidebar] = useState(false);
-  
-    const showSidebar = () => setSidebar(!sidebar);
-  
-    return (
-      <>
-        
-  
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
       <IconContext.Provider value={{ color: '#fff' }}>
-          <div className='navbar'>
-          <h1>Imona Game</h1>
+        <div className='navbar'>
           <Link to='#' className='menu-bars'>
-              <FaIcons.FaBars onClick={showSidebar} />
-            </Link>
-          </div>
-          <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-            <ul className='nav-menu-items' onClick={showSidebar}>
-            <li className='navbar-toggle' style={{"justifyContent: start"}}>
-                <Link to='#' className='menu-bars'>
-                  <AiIcons.AiOutlineClose />
-                </Link>
-              </li>
-              {SidebarData.map((item, index) => {
-                return (
-                  <li key={index} className={item.cName}>
-                    <Link to={item.path}>
-                      {item.icon}
-                      <span>{item.title}</span>
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-        </IconContext.Provider>
-      </>
-    );
-  }
-  
-  export default Navbar;
+            <FaIcons.FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+        <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
